19ed4638faa8656f170b83f49f9dc323f7c769f8
Manuel <manolo0726castro@gmail.com>
2025-05-16 07:21:13 -0500

    Primer proyecto java en git

---
+++ src/main/java/com/mycompany/online_store/Online_Store.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.online_store;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Online_Store {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
